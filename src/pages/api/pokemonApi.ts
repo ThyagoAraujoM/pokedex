c44639f99d97c955c224ff31e932911b1e4a7300
Thyago <thyagohyung@gmail.com>
2022-01-06 23:19:02 -0300

    recebendo dados da api

--- src/pages/api/pokemonApi.ts
+++ src/pages/api/pokemonApi.ts
@@ -1,11 +1,15 @@
 import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 
+type PokemonApi = {
+  name: string;
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   let response = await axios.get(`https://pokeapi.co/api/v2/pokemon/pikachu`);
-  console.log(response);
-  res.json(response.data);
+  console.log(response.data);
+  res.send(response.data);
 }
