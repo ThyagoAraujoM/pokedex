7b1aaf90fd6d5353e2c0e9aa0fefa45dafe9351b
Thyago <thyagohyung@gmail.com>
2022-01-23 22:08:02 -0300

    Adicionando página ao clicar em cima do pokemon

--- src/pages/pokemon/[id].tsx
+++ src/pages/pokemon/[id].tsx
@@ -4,18 +4,135 @@ import axios from "axios";
 import Link from "next/link";
 import styles from "../../styles/pokemon.module.scss";
 
+type PokemonData = {
+  name: string;
+  num: (num: string) => string;
+  image: string;
+  characteristics: {
+    height: string;
+    weight: string;
+    abilities: string;
+  };
+  types: { string };
+};
+
 export default function Pokemon() {
   const router = useRouter();
   const { id } = router.query;
-  const [pokemon, setPokemon] = useState({});
+  const [pokemon, setPokemon] = useState<PokemonData>();
+
+  function processNum(num) {
+    if (num <= 9) {
+      return "00" + num;
+    }
+    if (num <= 99) {
+      return "0" + num;
+    }
+    if (num >= 100) {
+      return num;
+    }
+  }
+
+  // async function getEvolutions(pokemon) {
+  //   let response = await axios.get(
+  //     `https://pokeapi.co/api/v2/evolution-chain/${pokemon.id}/`
+  //   );
+  //   let pokemonsData = response.data.chain;
+  //   let processedPokemon = "";
+  //   let actualEnvolvePhase = pokemonsData;
+
+  //   for (let i = 0; i < 2; i++) {
+  //     i -= 1;
+  //     if (processedPokemon != "") {
+  //       actualEnvolvePhase = actualEnvolvePhase.evolves_to;
+  //     }
+
+  //     let typesResponse = await axios.get(
+  //       `https://pokeapi.co/api/v2/type/${pokemon.id}/`
+  //     );
+  //     let typesData = typesResponse.data;
+  //     console.log(actualEnvolvePhase);
+  //     processedPokemon += `
+  //           <div>
+  //             <img src='https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${
+  //               actualEnvolvePhase.id
+  //             }.png' alt='' />
+  //             <p>
+  //               ${actualEnvolvePhase.species.name} <span> Nº ${processNum(
+  //       actualEnvolvePhase.id
+  //     )}</span>
+  //             </p>
+
+  //             <div>
+
+  //           </div>`;
+  //     // ${typesData.name.map((type) => {
+  //     //   return `<p>${type}</p>`;
+  //     // })}
+  //   }
+
+  //   return `
+  //         <div>
+  //           <h2>Evoluções</h2>
+  //           ${processNum}
+  //           </div>
+
+  //         </div>`;
+  // }
+
+  function createPokemonCard(filteredPokemon) {
+    return ` <img src=${filteredPokemon.image} alt='' />
+        <div>
+          <p>
+            Height <span>${filteredPokemon.characteristics.height}m</span>{" "}
+          </p>
+          <p>
+            Weight <span>${filteredPokemon.characteristics.weight}kg</span>{" "}
+          </p>
+          <p>
+          ${filteredPokemon.characteristics.abilities.map((abilityData) => {
+            return `Abilities <span>${abilityData.ability.name}</span>`;
+          })}
+          </p>
+        </div>
+        <div>
+          <div>
+            <h3>Type</h3>
+            ${filteredPokemon.types.map((dataType) => {
+              return `<p class="${dataType.type.name}">${dataType.type.name}</p>`;
+            })}
+          </div>
+          <div>
+            <h3>Weaknesses</h3>
+            ${filteredPokemon.types.map((typeInfo) => {})}
+          </div>
+        </div>
+        
+        </div>
+    `;
+  }
+
   useEffect(() => {
     async function getPokemon() {
       let response = await axios.get(`/api/searchPokemon?pokemon=${id}`);
       let data = response.data;
 
       let filteredPokemon = {
+        id: data.id,
         name: data.name,
+        num: processNum(data.order),
+        image: data.sprites.other.dream_world.front_default,
+        characteristics: {
+          height: data.height,
+          weight: data.weight,
+          abilities: data.abilities,
+        },
+        types: data.types,
       };
+
+      let cardPokemon = await createPokemonCard(filteredPokemon);
+
+      // setPokemon(cardPokemon);
     }
     if (id) {
       getPokemon();
@@ -33,84 +150,9 @@ export default function Pokemon() {
           Venusaur N°003
           <img src='' alt='' />
         </button>
-        <h2>Ivysaur N°002</h2>
+        <h2>{pokemon != null ? `${pokemon.name} N° ${pokemon.num}` : null}</h2>
       </header>
-      <main>
-        <img src='' alt='' />
-        <div>
-          <p>
-            When the bulb on its back grows large, it appears to lose the
-            ability to stand on its hind legs.
-          </p>
-          <div>
-            <p>Versions:</p>
-            <button>Blue ball</button>
-            <button>Red ball</button>
-          </div>
-        </div>
-        <div>
-          <p>
-            Height <span>1.0m</span>{" "}
-          </p>
-          <p>
-            Category <span>Seed</span>{" "}
-          </p>
-          <p>
-            Weight <span>13.0kg</span>{" "}
-          </p>
-          <p>
-            Abilities <span>Overgrow</span>{" "}
-          </p>
-          <p>
-            Gender <span>M F</span>{" "}
-          </p>
-        </div>
-        <div>
-          <div>
-            <h3>Type</h3>
-            <p>Grass</p>
-            <p>Poison</p>
-          </div>
-          <div>
-            <h3>Weaknesses</h3>
-            <p>Fire</p>
-            <p>Psychic</p>
-          </div>
-        </div>
-        <div>
-          <h2>Evoluções</h2>
-          <div>
-            <img src='' alt='' />
-            <p>
-              Bulbasaur <span> Nº 001</span>
-            </p>
-            <div>
-              <p>Grass</p>
-              <p>Poison</p>
-            </div>
-          </div>
-          <div>
-            <img src='' alt='' />
-            <p>
-              Bulbasaur <span> Nº 001</span>
-            </p>
-            <div>
-              <p>Grass</p>
-              <p>Poison</p>
-            </div>
-          </div>
-          <div>
-            <img src='' alt='' />
-            <p>
-              Bulbasaur <span> Nº 001</span>
-            </p>
-            <div>
-              <p>Grass</p>
-              <p>Poison</p>
-            </div>
-          </div>
-        </div>
-      </main>
+      <main className={styles.main}></main>
     </>
   );
 }
